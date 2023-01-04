762034049a28952e639196d4cee802858dd7333f
Stanislav Telyakaev <stelyakaev@usetech.ru>
2023-01-04 20:35:05 +0300

    added JUnit5Examples

---
+++ src/test/java/JUnit5Examples.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnit5Examples {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("### @BeforeAll !");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("### @AfterAll !");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        //open ("google.com")
+        System.out.println("### @BeforeEach !");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("### @AfterEach !");
+    }
+
+    @Test
+    void firstTest (){
+        System.out.println("###    @Test firstTest () !");
+        assertTrue (3 > 2);
+    }
+
+    @Test
+    void secondTest (){
+        System.out.println("###    @Test secondTest () !");
+        assertFalse (3 < 2);
+    }
+}
